fdc6229e2348636419771ca65fb6577af52f9938
Gabriel Rendon <gabriel.rendongmx@gmail.com>
2024-11-08 14:18:26 +0100

    Adicion a la pagina secundaria de seleccionar la categoria, sigue sin estar con las funcionalidades de sql

--- lib/menus/segunda_pag.dart
+++ lib/menus/segunda_pag.dart
@@ -1,11 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:random_mixer_app/clases/categorias.dart';
 import 'package:random_mixer_app/clases/estructura_basica.dart';
 import 'package:random_mixer_app/clases/Anuncios.dart';
-import 'package:random_mixer_app/clases/Manager.dart';
+import 'package:random_mixer_app/clases/nameList.dart';
+import 'package:random_mixer_app/clases/text_form.dart';
 
-
-class SegundaPag extends StatelessWidget {
+class SegundaPag extends StatefulWidget {
   const SegundaPag({super.key});
+
+  @override
+  _SegundaPagState createState() => _SegundaPagState();
+}
+
+class _SegundaPagState extends State<SegundaPag> {
+  List<String> names = [];
+
+  // Función para agregar nombre a la lista
+  void addName(String name) {
+    setState(() {
+      names.add(name);
+    });
+  }
+
+  // Función para eliminar nombre de la lista
+  void removeName(String name) {
+    setState(() {
+      names.remove(name);
+    });
+  }
+
+  Padding ManagerList() {
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextForm(addName: addName), // Paso la función para agregar nombres
+          
+          const CategorySelector(),
+
+          const SizedBox(height: 5),
+          const Text(
+            "Members",
+            style: TextStyle(
+                fontSize: 24, fontWeight: FontWeight.bold, color: Colors.blue),
+          ),
+          
+          const SizedBox(height: 10),
+          NameList(
+              names: names,
+              removeName:
+                  removeName), // Paso la lista y la función para eliminar
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,13 +64,13 @@ class SegundaPag extends StatelessWidget {
             child: Anuncios(),
           ),
         ),
-      body: const EstructuraBasica(widgetList: [NameManager()]),
-      bottomNavigationBar:const Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Anuncios(),
-          ],
-        ),
+      body: EstructuraBasica(widgetList: [ManagerList()]),
+      bottomNavigationBar: const Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Anuncios(),
+        ],
+      ),
     );
   }
 }
